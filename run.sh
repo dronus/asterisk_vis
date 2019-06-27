cc369f1d8e8cca610af6e96a53315321ae55bda8
dronus <paul.geisler@web.de>
2019-06-27 13:40:02 +0200

    refined start script (wait for X bootup, disable screensaver, chromium messages)

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-xscreensaver-command -exit
-DISPLAY=:0 chromium-browser  --remote-debugging-port=9222 --kiosk --disable-infobars  --disable-web-security --user-data-dir=tmp index.html
+cd `dirname "$0"`
+
+sleep 30
+
+DISPLAY=:0 xscreensaver-command -exit
+DISPLAY=:0 xset s off
+DISPLAY=:0 xset -dpms
+DISPLAY=:0 chromium-browser  --remote-debugging-port=9222 --kiosk --disable-infobars  --disable-web-security --user-data-dir=tmp --incognito index.html
 
