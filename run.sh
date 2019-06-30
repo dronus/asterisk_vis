2da1e13431234daa8684a42d828f033c81f284ca
dronus <paul.geisler@web.de>
2019-06-30 12:54:24 +0200

    run.sh resilient restart, forces chromium to restart every two hours to prevent memory runaway on small memory machines

--- run.sh
+++ run.sh
@@ -7,5 +7,7 @@ sleep 30
 DISPLAY=:0 xscreensaver-command -exit
 DISPLAY=:0 xset s off
 DISPLAY=:0 xset -dpms
-DISPLAY=:0 chromium-browser  --remote-debugging-port=9222 --kiosk --disable-infobars  --disable-web-security --user-data-dir=tmp --incognito index.html
-
+while true
+do
+	DISPLAY=:0 timeout -k30 2h chromium-browser --remote-debugging-port=9222 --kiosk --disable-infobars  --disable-web-security --user-data-dir=tmp --incognito index.html
+done
