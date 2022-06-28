b00e88e6c304181dc6434ea18b00c8aad2fbfc4c
dronus <paul.geisler@web.de>
2022-06-28 23:03:53 +0200

    run.sh use local server

--- run.sh
+++ run.sh
@@ -4,10 +4,12 @@ cd `dirname "$0"`
 
 sleep 30
 
+python3 -m http.server 8082  &
+
 DISPLAY=:0 xscreensaver-command -exit
 DISPLAY=:0 xset s off
 DISPLAY=:0 xset -dpms
 while true
 do
-	DISPLAY=:0 timeout -k30 2h chromium-browser --remote-debugging-port=9222 --kiosk --disable-infobars  --disable-web-security --user-data-dir=tmp --incognito index.html
+	DISPLAY=:0 timeout -k30 2h chromium-browser --remote-debugging-port=9222 --kiosk --disable-infobars  --disable-web-security --user-data-dir=tmp http://localhost:8082/index.html
 done
